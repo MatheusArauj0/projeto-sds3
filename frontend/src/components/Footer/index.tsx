64f75c219fb3e19afcd5363dbc7ddc68bcebe17c
Matheus Araujo <matheus_bk123@hotmail.com>
2021-05-05 20:34:41 -0300

    change footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,8 +3,9 @@ function Footer() {
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/MatheusArauj0" target="_blank" rel="noreferrer">Matheus de Araujo</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light"><small><strong>Spring React</strong><br />
+            Contatos: <a href="https://www.instagram.com/matheusarauj0_/" target="_blank" rel="noreferrer">Instagram</a> |
+            <a href="https://twitter.com/MatheusArauj0_" target="_blank" rel="noreferrer"> Twitter</a></small></p>
             </div>
         </footer>
     );
