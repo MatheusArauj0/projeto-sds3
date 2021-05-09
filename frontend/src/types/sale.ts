7721606e40cd2d7d859c145125c6f3a9500c9972
Matheus Araujo <matheus_bk123@hotmail.com>
2021-05-09 19:29:03 -0300

    Pagination

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,27 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type Salepage = {
+    content?: Sale [];
+    totalPages: number;
+    totalElements?: number;
+    last: boolean;
+    numberOfElements: number;
+    first: boolean;
+    size?: number;
+    number: number;
+    empty?: boolean;
+}
+
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
